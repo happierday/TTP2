255359953c8328628c44e615de297db51b359cd5
jian <jianchengw26@gmail.com>
2017-09-30 19:03:37 -0400

    remove all debugging

--- CalendarBackEnd/route/index.js
+++ CalendarBackEnd/route/index.js
@@ -5,15 +5,12 @@ const route = express.Router();
 
 route.get('/',(req,res)=>{
     let event = [];
-
     eventDetail.find({},(err,allEvent)=>{
-        console.log(allEvent);
         allEvent.forEach((events)=>{
             event[events.id-1] = events;
         })
         res.render('home/home',{events:event,error:req.params.error});
     })
-    console.log(event);
 })
 
 module.exports = route;
